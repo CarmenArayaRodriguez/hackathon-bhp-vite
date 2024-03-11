afaeb0f3a3b50f0975055d270c183d1ec5d55011
Fernanda Acuña <fdafilgueira@gmail.com>
2024-03-11 17:31:31 -0300

    login a deploy

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import { BasicTextFields, BasicPassword, BasicButtons } from './ComponentesLogin';
 import BhpHeader from './components/jsx/bhp-header';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
-
-function BasicForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+export default function AlertDialog() {
+  const [open, setOpen] = React.useState(false);
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const navigate = useNavigate();
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -25,10 +33,26 @@ function BasicForm() {
     setPassword('');
   };
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault(); // Evitar que se recargue la página al enviar el formulario
-    handleSaveToLocalStorage();
-    handleClick();
+    
+    // Validar si el nombre de usuario y la contraseña están completos
+    if (username.trim() === '' || password.trim() === '') {
+        setOpen(true); // Abrir el diálogo de alerta
+        return; // Detener la ejecución de la función si no se completan los campos
+    }
+
+    handleSaveToLocalStorage(); // Guardar en localStorage
+    handleClick(); // Manejar el clic
+    navigate("/subirArchivo"); // Redirigir al usuario
   };
 
   const handleClick = () => {
@@ -46,19 +70,31 @@ function BasicForm() {
     <div>
       <BhpHeader />
       <section className='login-form'>
-        <h1 >Iniciar sesión</h1>
+        <h1 className='H1'>Iniciar sesión</h1>
         <form onSubmit={handleSubmit}>
           <BasicTextFields value={username} onChange={handleUsernameChange} />
           <BasicPassword value={password} onChange={handlePasswordChange} />
-          <Link to="/subirArchivo">
-            <BasicButtons onClick={handleClick}>Texto del botón</BasicButtons>
-          </Link>
+          <BasicButtons ></BasicButtons>
         </form>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Atención"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Por favor complete todos los campos antes de enviar el formulario.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} autoFocus>
+              Volver
+            </Button>
+          </DialogActions>
+        </Dialog>
       </section>
     </div>
-
-
   );
 }
-
-export default BasicForm;
